b0e1cd37d5ccd7636b36f29f69cc16d58b0d812e
John Ranel Dimaculangan <johnranel.dimaculangan@gmail.com>
2025-06-23 23:01:30 +1000

    use Link from router-dom instead of a tag

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import "../App.css";
 import { PiMicrophoneStageFill, PiMusicNotesSimpleFill, PiClockClockwise, PiPlaylistFill } from "react-icons/pi";
 import { FaSpotify } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Header = ({ displayNameInitial }) => {
     return (
@@ -12,22 +13,22 @@ const Header = ({ displayNameInitial }) => {
                 </div>
                 <ul className="group/nav w-full grid grid-flow-col auto-cols-fr text-neutral-100">
                     <li className="py-5 sm:py-10 px-2 lg:px-5 hover:bg-lime-400 border-t-2 md:border-b-2 border-neutral-800 hover:bg-opacity-20 hover:border-t-2 md:hover:border-b-2 hover:border-lime-300">
-                        <a href="/top-artists" className="text-xs sm:text-sm lg:text-lg grid lg:flex gap-2 items-center justify-items-center lg:justify-center"><PiMicrophoneStageFill className="text-[25px]" /> TOP ARTISTS</a>
+                        <Link to="/top-artists" className="text-xs sm:text-sm lg:text-lg grid lg:flex gap-2 items-center justify-items-center lg:justify-center"><PiMicrophoneStageFill className="text-[25px]" /> TOP ARTISTS</Link>
                     </li>
                     <li className="py-5 sm:py-10 px-2 lg:px-5 hover:bg-lime-400 border-t-2 md:border-b-2 border-neutral-800 hover:bg-opacity-20 hover:border-t-2 md:hover:border-b-2 hover:border-lime-300">
-                        <a href="/top-tracks" className="text-xs sm:text-sm lg:text-lg grid lg:flex gap-2 items-center justify-items-center lg:justify-center"><PiMusicNotesSimpleFill className="text-[25px]" /> TOP TRACKS</a>
+                        <Link to="/top-tracks" className="text-xs sm:text-sm lg:text-lg grid lg:flex gap-2 items-center justify-items-center lg:justify-center"><PiMusicNotesSimpleFill className="text-[25px]" /> TOP TRACKS</Link>
                     </li>
                     <li className="py-5 sm:py-10 px-2 lg:px-5 hover:bg-lime-400 border-t-2 md:border-b-2 border-neutral-800 hover:bg-opacity-20 hover:border-t-2 md:hover:border-b-2 hover:border-lime-300">
-                        <a href="/recent" className="text-xs sm:text-sm lg:text-lg grid lg:flex gap-2 items-center justify-items-center lg:justify-center"><PiClockClockwise className="text-[25px]" /> RECENT</a>
+                        <Link to="/recent" className="text-xs sm:text-sm lg:text-lg grid lg:flex gap-2 items-center justify-items-center lg:justify-center"><PiClockClockwise className="text-[25px]" /> RECENT</Link>
                     </li>
                     <li className="py-5 sm:py-10 px-2 lg:px-5 hover:bg-lime-400 border-t-2 md:border-b-2 border-neutral-800 hover:bg-opacity-20 hover:border-t-2 md:hover:border-b-2 hover:border-lime-300">
-                        <a href="/playlist" className="text-xs sm:text-sm lg:text-lg grid lg:flex gap-2 items-center justify-items-center lg:justify-center"><PiPlaylistFill className="text-[25px]" /> PLAYLIST</a>
+                        <Link to="/playlist" className="text-xs sm:text-sm lg:text-lg grid lg:flex gap-2 items-center justify-items-center lg:justify-center"><PiPlaylistFill className="text-[25px]" /> PLAYLIST</Link>
                     </li>
                 </ul>
                 <div className="w-[50px] ml-10 hidden md:block">
-                    <a href="/" className="w-[50px] h-[50px] bg-teal-300 rounded-full flex items-center justify-center">
+                    <Link to="/" className="w-[50px] h-[50px] bg-teal-300 rounded-full flex items-center justify-center">
                         <span className="text-2xl font-bold">{displayNameInitial}</span>
-                    </a>
+                    </Link>
                 </div>
             </nav>
         </header>
--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import { Link } from "react-router-dom";
 
 const Main = ({ children }) => {
     const path = window.location.pathname;
@@ -14,9 +15,9 @@ const Main = ({ children }) => {
     return (
         <main className="bg-gradient-to-b grid from-neutral-500 via-neutral-800 to-neutral-900">
             <Header displayNameInitial={displayNameInitial} />
-            <a href="/" className="place-self-center mt-10 w-[50px] h-[50px] block md:hidden bg-teal-300 rounded-full flex items-center justify-center">
+            <Link to="/" className="place-self-center mt-10 w-[50px] h-[50px] block md:hidden bg-teal-300 rounded-full flex items-center justify-center">
                 <span className="text-2xl font-bold">{displayNameInitial}</span>
-            </a>
+            </Link>
             <section className="grid order-2 md:order-1 py-10">
                 <div className={`container px-10 ${path === "/" ? "flex flex-col gap-5 items-center justify-center py-20" : "pb-10" } place-self-center bg-neutral-100 bg-opacity-10 rounded-xl min-h-screen`}>
                     {children}
